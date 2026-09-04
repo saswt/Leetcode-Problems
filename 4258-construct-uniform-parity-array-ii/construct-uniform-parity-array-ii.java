7a4eaefacb9ce38310b881c688a25f1fd63038eb
Shashwat Shahi <120392889+saswt@users.noreply.github.com>
2026-09-04 07:36:45 +0530

    Time: 2 ms (100.00%) | Memory: 121.6 MB (54.55%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
@@ -1,3 +1,9 @@
+//optimized Approach
+//first we will check we have to change parity of nums1
+//if it changes, subtraction is only allowed with a smaller element
+//condition: 1st element(or minEl)%2 ==1
+//1st element is odd, retrun true kar denge
+
 class Solution {
     public boolean uniformArray(int[] nums1) {
         int minEl = Integer.MAX_VALUE;
